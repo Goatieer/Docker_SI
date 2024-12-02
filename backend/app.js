5e29552af363546d0690ed845e91fdeb1e6964eb
axteure <clement.hanquiez@gmail.com>
2024-12-02 16:29:17 +0100

    docker fonctionnel

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,21 @@ const app = express();
 const port = 5001;
 
 // Crée une connexion à la base de données MySQL (actuellement en commentaire)
-/*
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
-*/
+
+db.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données :", err);
+    return;
+  }
+  console.log("Connecté à la base de données MariaDB !");
+});
 
 // Crée un client Modbus
 const client = new ModbusRTU();
